fd7f62821c6f842cf4dea17effc3c1a309d34be2
sekhonconsultants <sekhonconsultants@outlook.com>
2017-07-29 11:56:07 -0700

    Update validation-functions.js

--- validation-functions.js
+++ validation-functions.js
@@ -50,7 +50,7 @@ $(document).ready(function() {
         // show the loading 
         $('#btnPostForm').prepend($('<span></span>').addClass('glyphicon glyphicon-refresh glyphicon-refresh-animate'));
         var jqxhr = $.post(url, $form.serialize(), function(data) {
-            console.log("Success! Data: " + data.statusText);
+            console.log("Success! Data: " + data);
             //$(location).attr('href',redirectUrl);
         })
             .fail(function(data) {
